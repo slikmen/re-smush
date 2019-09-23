286ca09e5146385a679b3510e561c2acb64924f2
Daan Seegers <daan@raow.nl>
2019-09-23 10:25:13 +0200

    Update Service

--- src/Service.php
+++ src/Service.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace OffbeatWP\SocialImporter;
+
+use OffbeatWP\Services\AbstractService;
+use Illuminate\Support\Collection;
+
+class Service extends AbstractService
+{
+
+    public function register()
+    {
+    }
+
+}
