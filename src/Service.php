ddb7db17ef7a4eff23df4d32e33859652fe7669e
Daan Seegers <daan@raow.nl>
2019-09-24 15:55:59 +0200

    Add 2 new function, one to get the Basepath (time), the other one to get the file location

--- src/Service.php
+++ src/Service.php
@@ -11,14 +11,15 @@ class Service extends AbstractService
     public function register()
     {
 //        add_filter('wp_handle_upload', [$this, 'handleUpload'], 10, 2);
-
         add_filter('wp_generate_attachment_metadata', [$this, 'handleThumbnails'], 10, 2);
+
+        $this->defaultQuality = 90;
     }
 
     public function handleUpload($image)
     {
         $apiCall = new SmushImage($image['type'], $image['file']);
-        $apiCall->quality = 90;
+        $apiCall->quality = $this->defaultQuality;
         $apiCall->execute();
 
         return $image;
@@ -26,14 +27,21 @@ class Service extends AbstractService
 
     public function handleThumbnails($image, $key)
     {
-
-        $basePath = substr($image["file"], 0, strrpos($image["file"], '/'));
-        $file = wp_upload_dir()['basedir'] . '/' . $basePath . '/' . $image["sizes"]["thumbnail"]["file"];
-        $apiCall = new SmushImage(get_post_mime_type($key) , $file);
-        $apiCall->quality = 90;
+        $apiCall = new SmushImage(get_post_mime_type($key), $this->getFile($image, $size = 'thumbnail'));
+        $apiCall->quality = $this->defaultQuality;
         $apiCall->execute();
 
         return $image;
+    }
+
+    public function getBasePath($image)
+    {
+        return substr($image["file"], 0, strrpos($image["file"], '/'));
+    }
+
+    public function getFile($image, $size = 'thumbnail')
+    {
+        return wp_upload_dir()['basedir'] . '/' . $this->getBasePath($image) . '/' . $image["sizes"][$size]["file"];
     }
 
 }
